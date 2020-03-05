812ce44476eb30072f2be899af9ee9155e43d31b
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2020-03-05 02:43:25 +0200

    js ready and tested

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Java");
+function loadDoc() { //src:https://www.w3schools.com/xml/tryit.asp?filename=tryajax_get
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("data").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "/ajax", true);
+    xhttp.send();
+}
+setInterval(loadDoc, 500);//reloads the html 2hz
